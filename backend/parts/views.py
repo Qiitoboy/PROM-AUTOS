43b418d2a166f10d07c6244278b4ea1cedfb9b6f
RansfordTakyi <roduro_takyi@st.ug.edu.gh>
2026-07-08 21:31:57 +0000

    search, filtering and ordering

--- backend/parts/views.py
+++ backend/parts/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Part
 from .serializers import PartSerializer
@@ -10,6 +12,28 @@ class PartViewSet(viewsets.ModelViewSet):
 
     queryset = Part.objects.all().order_by("-created_at")
     serializer_class = PartSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "category",
+    ]
+
+    search_fields = [
+        "name",
+        "compatibility",
+        "description",
+    ]
+
+    ordering_fields = [
+        "price",
+        "stock",
+        "created_at",
+    ]
 
 
     def get_permissions(self):
